fdb74a8cddfb88b6e5afb0425bbb66bcba38ef49
Yash Kangane <yashkangane0307@gmail.com>
2026-03-02 11:29:11 +0530

    files modified

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 
 const port = 3000;
-
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.listen(port, ()=>{
